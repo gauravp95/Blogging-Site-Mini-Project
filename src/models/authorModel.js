59161d7d7af61e15408cdc24b283b4565f6da3f8
gaurav95 <gauravpatil4195@gmail.com>
2022-04-27 10:41:35 +0530

    email vaidation updated in author model

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
+const validator = require("validator")
 
 const authorModel = new mongoose.Schema(
   {
     fname: { type: String, required: true },
     lname: { type: String, required: true },
     title: { type: String, enum: ["Mr", "Mrs", "Miss"], required: true },
-    email: { type: String, unique: true },
+    email: { type: String, unique: true, validate:{
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email',
+      isAsync: false
+    }},
     password: { type: String, required: true },
   },
   { timestamps: true }
